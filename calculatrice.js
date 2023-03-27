dc53df7d98795c4d594433527dbcdb05c9b7c582
Remfich <remy.ficheux@student.junia.com>
2023-03-27 13:17:31 +0200

    etape 2 fini

--- calculatrice.js
+++ calculatrice.js
@@ -1,23 +1,37 @@
 class Calculatrice {
-    calculer() {
-      let c = document.getElementById("output").value;
-      let d = eval(c);
-      document.getElementById("output").value = d;
-      document.getElementById("output2").value = c+" = "+d;
-    }
-    
-    afficher(val) {
-      document.getElementById("output").value += val;
-    }
-    
-    effacer() {
-      document.getElementById("output").value = "";
-    }
 
-    retourArriere() {
-        let output = document.getElementById("output");
-        output.value = output.value.slice(0, -1); // supprime le dernier caractÃ¨re de la valeur de sortie
-    }
+  constructor() {
+    this.historique = [];
+    this.nextEval = false;
+ }
 
+  calculer() {
+    let c = document.getElementById("output").value;
+    let d = eval(c);
+    document.getElementById("output").value = d;
+    document.getElementById("output2").value = c+" = "+d;
+    this.historique.unshift(c);
+    this.nextEval = true;
   }
-  let calc = new Calculatrice();
+  
+  afficher(val) {
+    document.getElementById("output").value += val;
+    this.nextEval = false;
+  }
+  
+  effacer() {
+    document.getElementById("output").value = "";
+    this.nextEval = false;
+  }
+
+  retourArriere() {
+    if(this.nextEval==true){
+          document.getElementById("output").value = this.historique[0];
+          this.nextEval = false;
+    }else{
+      document.getElementById("output").value = document.getElementById("output").value.slice(0,-1);
+      this.nextEval = false;
+      }
+  }
+}
+let calc = new Calculatrice();
