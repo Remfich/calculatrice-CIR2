8ad6de29a8e38cb131fd62bd381de49afc13a3fc
Remfich <remy.ficheux@student.junia.com>
2023-03-27 11:16:05 +0200

    class + history + back

--- calculatrice.js
+++ calculatrice.js
@@ -1,16 +1,23 @@
-function calculer() 
-{ 
-    let c = document.getElementById("output").value 
-    let d = eval(c) 
-    document.getElementById("output").value = d 
-} 
-//fonction qui affiche la valeur
-function afficher(val) 
-{ 
-    document.getElementById("output").value+=val 
-} 
-//fonction qui efface l'écran 
-function effacer() 
-{ 
-    document.getElementById("output").value = "" 
-}
+class Calculatrice {
+    calculer() {
+      let c = document.getElementById("output").value;
+      let d = eval(c);
+      document.getElementById("output").value = d;
+      document.getElementById("output2").value = c+" = "+d;
+    }
+    
+    afficher(val) {
+      document.getElementById("output").value += val;
+    }
+    
+    effacer() {
+      document.getElementById("output").value = "";
+    }
+
+    retourArriere() {
+        let output = document.getElementById("output");
+        output.value = output.value.slice(0, -1); // supprime le dernier caractÃ¨re de la valeur de sortie
+    }
+
+  }
+  let calc = new Calculatrice();
